8ec62b3cdb001754fd57ab2041084fa8124d3620
mohammadaboelnasr <m.aboelnasr@live.com>
2023-01-16 23:11:27 +0300

    test riverpod

--- lib/prov.dart
+++ lib/prov.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'prov.g.dart';
@@ -15,4 +18,24 @@ String name(ref) {
 @riverpod
 DateTime myDate(ref) {
   return DateTime.now();
+}
+
+class Clock extends StateNotifier<DateTime> {
+  // 1. initialize with current time
+  Clock() : super(DateTime.now()) {
+    // 2. create a timer that fires every second
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      // 3. update the state with the current time
+      state = DateTime.now();
+    });
+  }
+
+  late final Timer _timer;
+
+  // 4. cancel the timer when finished
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 }
